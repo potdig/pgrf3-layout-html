6fec4d07f11b7238717ef87fea246a55511b1489
potdig <potadig.ashnerd.zino@gmail.com>
2021-08-09 02:34:35 +0900

    blink duration

--- src/components/setup/BlinkBox.vue
+++ src/components/setup/BlinkBox.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="box"></div>
+  <div class="box" :style="{ animationDuration: `${duration}s` }"></div>
 </template>
 
 <script setup>
+import { ref } from "vue"
 
+const duration = ref(Math.random() * (15 - 5) + 5)
 </script>
 
 <style lang="scss" scoped>
 .box {
-  animation: 5s infinite fade-out ;
+  animation: 5s linear infinite fade-out;
   margin: 8px;
   width: 200px;
   height: 200px;
