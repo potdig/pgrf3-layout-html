a5e5ab713c2a326da2f0d4a0eee9fe7f502fe932
potdig <potadig.ashnerd.zino@gmail.com>
2021-08-09 02:21:08 +0900

    animation for BlinkBox

--- src/components/setup/BlinkBox.vue
+++ src/components/setup/BlinkBox.vue
@@ -8,10 +8,21 @@
 
 <style lang="scss" scoped>
 .box {
+  animation: 5s infinite fade-out ;
   margin: 8px;
   width: 200px;
   height: 200px;
   background-color: lightgray;
   border-radius: 16px;
+}
+
+@keyframes fade-out {
+  from {
+    opacity: 100%;
+  }
+
+  to {
+    opacity: 0%;
+  }
 }
 </style>
