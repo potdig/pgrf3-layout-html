f1cee59f8c87f6d7f4d312e229e27edce38a48e8
potdig <potadig.ashnerd.zino@gmail.com>
2021-08-09 02:38:55 +0900

    complete background

--- src/components/setup/Background.vue
+++ src/components/setup/Background.vue
@@ -1,50 +1,44 @@
 <template>
   <div id="wrapper">
+    <div id="filter"></div>
     <div id="boxes">
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
-      <BlinkBox></BlinkBox>
+      <BlinkBox v-for="i in [...Array(boxCount)].map((_, i) => i)" :key="i"></BlinkBox>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import BlinkBox from './BlinkBox.vue'
+
+const boxCount = ref(60)
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 #wrapper {
-  position: absolute;
+  position: fixed;
+  overflow: hidden;
   width: 1920px;
   height: 1080px;
   z-index: -1;
 }
 
+#filter {
+  position: absolute;
+  z-index: -2;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom, white 30%, rgba(0, 0, 0, 0));
+}
+
 #boxes {
-  width: 1200px;
+  position: relative;
+  z-index: -3;
+  width: 2200px;
   display: flex;
   flex-wrap: wrap;
+  transform: translate(-110px, 60px) rotate(-15deg);
+  filter: blur(6px);
 }
 
 p {
