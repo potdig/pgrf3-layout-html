39ce38e2bb156f66880db673a29159dce7b054f4
potdig <potadig.ashnerd.zino@gmail.com>
2021-08-11 01:11:16 +0900

    footer

--- src/Setup.vue
+++ src/Setup.vue
@@ -1,37 +1,39 @@
 <template>
   <Background></Background>
-  <div id="setup">
-    <header>
-      <Header></Header>
-    </header>
-    <main>
-      <!-- TODO: schedule -->
-      <!-- TODO: tweet -->
-      <span>This is foreground</span>
-      <p>This is foreground</p>
-      <p>This is foreground</p>
-      <p>This is foreground</p>
-      <p>This is foreground</p>
-      <p>This is foreground</p>
-      <p>This is foreground</p>
-      <p>This is foreground</p>
-      <p>This is foreground</p>
-    </main>
-    <footer>
-      <!-- TODO: footer -->
-    </footer>
-  </div>
+  <header>
+    <Header></Header>
+  </header>
+  <main>
+    <!-- TODO: schedule -->
+    <!-- TODO: tweet -->
+    <span>This is foreground</span>
+    <p>This is foreground</p>
+    <p>This is foreground</p>
+    <p>This is foreground</p>
+    <p>This is foreground</p>
+    <p>This is foreground</p>
+    <p>This is foreground</p>
+    <p>This is foreground</p>
+    <p>This is foreground</p>
+  </main>
+  <footer>
+    <Footer></Footer>
+  </footer>
 </template>
 
 <script setup>
 import Background from './components/setup/Background.vue'
 import Header from './components/setup/Header.vue'
+import Footer from './components/setup/Footer.vue'
 </script>
 
 <style>
 @import '/src/assets/common.css';
 
-#setup {
+#root {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
 }
@@ -42,6 +44,14 @@ header {
   justify-content: flex-start;
   align-items: center;
   height: 120px;
+  padding: 0 48px;
+}
+
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 80px;
   padding: 0 48px;
 }
 </style>
---
+++ src/components/setup/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="spotify">
+    <span>Now playing: Music Title / Artist Name</span>
+  </div>
+  <div id="hashtag-guide">hashtag: <span>#PGRF</span></div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+$spotifyHeight: 48px;
+
+#spotify {
+  display: flex;
+  align-items: center;
+  height: $spotifyHeight;
+  line-height: $spotifyHeight;
+  padding: 0 24px;
+  background-color: white;
+  border-radius: 24px;
+}
+
+#hashtag-guide {
+  font-family: 'Staatliches', sans-serif;
+  font-size: 2em;
+
+  span {
+    text-decoration: underline dashed;
+  }
+}
+</style>
