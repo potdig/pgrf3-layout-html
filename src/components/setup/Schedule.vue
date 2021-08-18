7cb0b26a030eda569fb3d85d179d7611a3492d26
potdig <potadig.ashnerd.zino@gmail.com>
2021-08-18 20:56:15 +0900

    Run components for each run

--- src/components/setup/Schedule.vue
+++ src/components/setup/Schedule.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="schedule">
-    <Run upNext></Run>
-    <Run></Run>
-    <Run></Run>
+    <Run v-for="(run, index) in runs" :key="index" :upNext="index === 0"></Run>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useStore } from 'vuex'
 import Run from './Run.vue'
+
+const store = useStore()
+const runs = ref(store.getters.runsOnSchedule)
 </script>
 
 <style lang="scss" scoped>
--- src/store/games.js
+++ src/store/games.js
@@ -2,5 +2,10 @@ import testData from './test-data/games.js'
 export default {
   state: () => ({
     games: testData
-  })
+  }),
+  getters: {
+    runsOnSchedule ({ games }) {
+      return games.slice(0, 3)
+    }
+  }
 }
