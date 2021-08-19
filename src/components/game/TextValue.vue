e09aa91d215e465035da40b1cde0f33608af06b1
potdig <potadig.ashnerd.zino@gmail.com>
2021-08-20 02:02:30 +0900

    icon for accounts

--- src/components/game/TextValue.vue
+++ src/components/game/TextValue.vue
@@ -3,9 +3,18 @@
     <p class="value">{{ value }}</p>
     <p class="account">
       <transition name="switch">
-        <span v-if="account.twitch">{{ account.twitch }}</span>
-        <span v-else-if="account.twitter">{{ account.twitter }}</span>
-        <span v-else-if="account.youtube">{{ account.youtube }}</span>
+        <span v-if="account.twitch">
+          <SvgIcon type="mdi" :path="mdiTwitch" size="1.1em" style="top: 0.1em"></SvgIcon>
+          {{ account.twitch }}
+        </span>
+        <span v-else-if="account.twitter">
+          <SvgIcon type="mdi" :path="mdiTwitter" size="1.1em" style="top: 0.1em"></SvgIcon>
+          {{ account.twitter }}
+        </span>
+        <span v-else-if="account.youtube">
+          <SvgIcon type="mdi" :path="mdiYoutube" size="1.1em" style="top: 0.2em"></SvgIcon>
+          {{ account.youtube }}
+        </span>
         <span v-else>-</span>
       </transition>
     </p>
@@ -14,6 +23,8 @@
 
 <script setup>
 import { defineProps } from 'vue'
+import { mdiTwitch, mdiTwitter, mdiYoutube } from '@mdi/js'
+import SvgIcon from '@jamescoyle/vue-icon'
 
 const props = defineProps({
   value: String,
@@ -25,7 +36,7 @@ const props = defineProps({
 </script>
 
 <style lang="scss" scoped>
-$accountHeight: 1.2em;
+$accountHeight: 1.3em;
 $sinWave: cubic-bezier(0.37, 0, 0.63, 1);
 
 div {
@@ -47,8 +58,13 @@ div {
   font-weight: bold;
   display: flex;
   justify-content: flex-end;
+  align-items: center;
   position: relative;
   overflow: hidden;
+
+  svg {
+    position: relative;
+  }
 
   span {
     text-align: right;
