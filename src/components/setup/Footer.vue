be257cf546464537cf5fd62376069c733b964be8
potdig <potadig.ashnerd.zino@gmail.com>
2022-09-29 00:06:58 +0900

    change hashtag to date on footer for setup

--- src/components/setup/Footer.vue
+++ src/components/setup/Footer.vue
@@ -2,13 +2,17 @@
   <div id="spotify">
     <span>Now playing: {{ musicInfo }}</span>
   </div>
-  <div id="hashtag-guide">hashtag: <span>#PGRF3</span></div>
+  <div id="date">Day {{ day }}: {{ date }}</div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
+import { DateTime } from 'luxon'
 
+const dateTime = ref(DateTime.now())
+const date = computed(() => dateTime.value.toFormat('yyyy/MM/dd'))
+const day = computed(() => Math.ceil(dateTime.value.diff(DateTime.fromSQL('2022-10-29'), 'days').days))
 const store = useStore()
 const musicInfo = computed(() => store.getters.shortSpotifyInfo)
 </script>
@@ -26,12 +30,8 @@ $spotifyHeight: 48px;
   border-radius: 24px;
 }
 
-#hashtag-guide {
+#date {
   font-family: 'Staatliches', sans-serif;
   font-size: 2em;
-
-  span {
-    text-decoration: underline dashed;
-  }
 }
 </style>
