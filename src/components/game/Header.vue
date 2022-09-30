10744ad3ec3d9329192156afab670b6034a2f696
potdig <potadig.ashnerd.zino@gmail.com>
2022-10-01 00:22:33 +0900

    dense header

--- src/components/game/Header.vue
+++ src/components/game/Header.vue
@@ -1,15 +1,18 @@
 <template>
-  <img id="logo" src="/assets/pgrf/images/pgrf2_logo_simple.png" />
-  <div id="title">
-    <p id="game-title">{{ info.title }}</p>
+  <img id="logo" :class="{ dense }" src="/assets/pgrf/images/pgrf2_logo_simple.png" />
+  <div id="title" :class="{ dense }">
+    <p id="game-title" :class="{ dense }">{{ info.title }}</p>
     <p id="category-platform">{{ info.category }} - {{ info.platform }}</p>
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, defineProps } from 'vue'
 import { useStore } from 'vuex'
 
+const props = defineProps({
+  dense: Boolean
+})
 const store = useStore()
 const info = computed(() => store.getters.headerInfo)
 </script>
@@ -18,26 +21,33 @@ const info = computed(() => store.getters.headerInfo)
 #logo {
   height: 100%;
   margin-right: calc(1080px * 0.02);
+
+  &.dense {
+    height: 60%;
+  }
 }
 
-header {
-  margin-left: 24px;
-  padding: 8px 0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  height: 100px;
-}
+#title {
+  &.dense {
+    display: flex;
+    align-items: center;
+  }
 
-#title p {
-  margin-top: -0.2em;
-  margin-bottom: 0;
-  line-height: 1.2em;
+  p {
+    margin-top: -0.2em;
+    margin-bottom: 0;
+    line-height: 1.2em;
+  }
 }
 
 #game-title {
   font-size: 1.9em;
   font-weight: bolder;
+
+  &.dense {
+    font-size: 1.6em;
+    margin-right: 1em;
+  }
 }
 
 #category-platform {
